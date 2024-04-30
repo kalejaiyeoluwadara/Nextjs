0da3355e30c9226d8e599d488cd47ca2f93abf1b
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-04-30 20:00:45 +0100

    done

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,9 @@
+import CreateForm from "./createForm"
+export default function createTicket() {
+  return (
+    <main>
+      <h2 className="text-primary text-center ">Add a New Ticket</h2>
+      <CreateForm />
+    </main>
+  );
+}
